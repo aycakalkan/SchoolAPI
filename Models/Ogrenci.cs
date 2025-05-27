08fe2257c6134aa4bf038b9b1ac5778b1a8de47a
aycakalkan <aycakalkan2@gmail.com>
2025-05-27 20:42:04 +0300

    entities rewritten and some requirements included

--- Models/Ogrenci.cs
+++ Models/Ogrenci.cs
@@ -1,14 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace SchoolApi.Models
 {
     public class Ogrenci
     {
         public int Id { get; set; }
-        public string Ad { get; set; }
 
-        public ICollection<OgrenciDers> OgrenciDersler { get; set; }
+        [Required(ErrorMessage = "Öğrenci adı zorunludur")]
+        [StringLength(100, ErrorMessage = "Ad en fazla 100 karakter olabilir")]
+        public string Ad { get; set; } = string.Empty;
+
+        public ICollection<OgrenciDers> OgrenciDersler { get; set; } = new List<OgrenciDers>();
     }
 }
-
-
--- Models/OgrenciDers.cs
+++ Models/OgrenciDers.cs
@@ -3,9 +3,9 @@
     public class OgrenciDers
     {
         public int OgrenciId { get; set; }
-        public Ogrenci Ogrenci { get; set; }
+        public Ogrenci Ogrenci { get; set; } = null!;
 
         public int DersId { get; set; }
-        public Ders Ders { get; set; }
+        public Ders Ders { get; set; } = null!;
     }
 }
