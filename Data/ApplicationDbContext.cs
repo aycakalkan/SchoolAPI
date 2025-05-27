39d3a02251e0c37afc9909f79d3518e268532d85
aycakalkan <aycakalkan2@gmail.com>
2025-05-27 20:42:43 +0300

    dbcontext modified by new structure

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,24 +1,42 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using SchoolApi.Models; // Model klasörüne göre değişebilir
+using SchoolApi.Models;
 
 namespace SchoolApi.Data
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options) { }
+            : base(options)
+        {
+        }
 
         public DbSet<Ogrenci> Ogrenciler { get; set; }
-        public DbSet<Ders> Dersler { get; set; }
         public DbSet<Ogretmen> Ogretmenler { get; set; }
+        public DbSet<Ders> Dersler { get; set; }
         public DbSet<OgrenciDers> OgrenciDersler { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // Configure composite key for OgrenciDers
             modelBuilder.Entity<OgrenciDers>()
                 .HasKey(od => new { od.OgrenciId, od.DersId });
 
-            base.OnModelCreating(modelBuilder);
+            // Configure many-to-many relationship
+            modelBuilder.Entity<OgrenciDers>()
+                .HasOne(od => od.Ogrenci)
+                .WithMany(o => o.OgrenciDersler)
+                .HasForeignKey(od => od.OgrenciId);
+
+            modelBuilder.Entity<OgrenciDers>()
+                .HasOne(od => od.Ders)
+                .WithMany(d => d.OgrenciDersler)
+                .HasForeignKey(od => od.DersId);
+
+            // Configure one-to-many relationship between Ogretmen and Ders
+            modelBuilder.Entity<Ders>()
+                .HasOne(d => d.Ogretmen)
+                .WithMany(o => o.Dersler)
+                .HasForeignKey(d => d.OgretmenId);
         }
     }
 }
