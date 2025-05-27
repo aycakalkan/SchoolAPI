f04ad54ea388a0ffe5edb664624da20787044c4e
aycakalkan <aycakalkan2@gmail.com>
2025-05-27 20:38:02 +0300

    Interfaces are initialized separately

---
+++ Core/Interfaces/IOgretmenService.cs
@@ -0,0 +1,13 @@
+﻿using SchoolApi.Models;
+
+namespace SchoolApi.Services.Interfaces
+{
+    public interface IOgretmenService
+    {
+        Task<IEnumerable<Ogretmen>> GetAllAsync();
+        Task<Ogretmen?> GetByIdAsync(int id);
+        Task<Ogretmen> CreateAsync(Ogretmen ogretmen);
+        Task<bool> UpdateAsync(Ogretmen ogretmen);
+        Task<bool> DeleteAsync(int id);
+    }
+}
